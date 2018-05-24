1f4e2bc416dc1d1f272668057930b6fc8cba7736
aaa <aaa@aaa.com>
2018-05-24 21:16:02 +0300

    d8 migration from d7 v0001

--- webform8_encrypt/src/WebformEncryptSubmissionStorage.php
+++ webform8_encrypt/src/WebformEncryptSubmissionStorage.php
@@ -31,8 +31,9 @@ class WebformEncryptSubmissionStorage extends WebformSubmissionStorage {
           $this->encryptChildren($data[$element_name], $encryption_profile);
         }
         else {
-          $encrypted_value = \Drupal::service('encryption')
-            ->encrypt($value, $encryption_profile);
+          //$encrypted_value = \Drupal::service('encryption')            ->encrypt($value, $encryption_profile);
+          $encrypted_value= my_encrypt($value,get_my_public_Key());
+
           // Save the encrypted data value.
           $data[$element_name] = $encrypted_value;
         }
@@ -79,11 +80,18 @@ class WebformEncryptSubmissionStorage extends WebformSubmissionStorage {
     if ($check_permissions && !\Drupal::currentUser()->hasPermission('view encrypted values')) {
       return '[Value Encrypted]';
     }
-
+    /*
     $decrypted_value = \Drupal::service('encryption')->decrypt($string, $encryption_profile);
     if ($decrypted_value === FALSE) {
       return $string;
     }
+    */
+
+    $decrypted_value = my_decrypt($string,get_my_private_Key());
+    if ($decrypted_value === FALSE) {
+      return $string;
+    }
+
 
     return $decrypted_value;
   }
@@ -169,4 +177,81 @@ class WebformEncryptSubmissionStorage extends WebformSubmissionStorage {
     }
   }
 
+}
+
+
+//
+//--------------------------------------
+// Encrypt data using the public key
+function my_encrypt($data, $publicKey)
+{
+    // Encrypt the data using the public key
+   openssl_public_encrypt($data, $encryptedData, $publicKey);
+  //$encryptedData=ssl_encrypt($data,'public',get_my_public_Key());
+//echo  "<HR>publicKey= $publicKey";
+    // Return encrypted data
+    return base64_encode($encryptedData);
+}
+
+// Decrypt data using the private key
+function my_decrypt($data, $privateKey)
+{
+    // Decrypt the data using the private key
+   openssl_private_decrypt(base64_decode($data), $decryptedData, $privateKey);
+//   echo  "<HR>privateKey = $privateKey";
+
+    // Return decrypted data
+    return ($decryptedData);
+}
+
+function get_my_private_Key()
+{
+$private_my_key= trim('-----BEGIN PRIVATE KEY-----
+MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJka97mFuKzAd9qY
+s5nKdoNW/dkyQCFcGFFtZ7ZsXvTUOe678Bh91Ca5zeYin+vpsYRbzci/CYvxbYui
+VJVjn+URmnY2/I1FzQg0NDkGFD27lgBAFB1euLf+hN6mhVBdj4y9V2cpHV9IiWAg
+5WZeF2d/nwuyMI/nv1mdmvfNmyofAgMBAAECgYARMk86vkNE3AVaPyaFEpGHJQE0
+IQnE9XQhyTkJ61grBZ9QKjnIdnGau+ndfGwb8k6nACy37IUk+8QDGAOKF/uAcaR5
+9pXArxlJo73EC9yadFyAc/DMf/y2vEFFC4ZrXoZhx+CG1qvqjSK4hccg2VZdlq0U
+ktXprIFXCwgxQkFMSQJBAMihyvPbAWzP8+uSdMcCBlzrkhIuokYhRZDoyYtsNq/Y
+Dsyg1N+4F0MGHcwikYhc68oZuHXe4A1bRkGDriAfk1MCQQDDW4V9xsRDM4QGKEKS
+lcUgK+FQP4pNKxa4T/peaXDeYTKIZ1KRFNUULfX57vFWJ9y857zkzQ+m0JksDfUM
+KeCFAkBlvCT9ooAGfSxit9EfWJeBZrKbJaqDhTuRT3ljiWvxns1iVeubxe2FvwQ9
+Dwpc60FlKcWvf2R9YVsuvWb+QwShAkBosPZD5i4tWUEqT48FoS+Hwxw29iY2np3J
+XkrtJ5JuCiabhMtxpnleHBdSvmMk6a3YqlPHRcFagzVir4Qf5Z1FAkEAjqJyHb8V
+rZu2uj1+r/vGv9BbH80xjReT8udGjewBxBtqCfFoMuE8Xh6EImOXdZSHBXF2Srzq
+bb12MrEizy/XNQ==
+-----END PRIVATE KEY-----');
+
+/*
+    $settings = _webform_pki_encrypt_webform_pki_encrypt_settings(get_current_node_id());
+    if (!empty($settings)) {
+      //$public_my_key = $settings['public_key'] ;
+      $private_my_key = $settings['private_key'];
+    }
+    $session_private_key=_mySite_session("current_private_key");
+    //drupal_set_message("SESS PRIV="._mySite_session("current_private_key"));
+    //drupal_set_message(  "<h1>nid=".get_current_node_id()."private_my_key=$private_my_key </h1>");
+    if($session_private_key!=null) $private_my_key=$session_private_key;
+*/
+    return $private_my_key;
+}
+
+function get_my_public_Key()
+{
+$public_my_key=trim('-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCZGve5hbiswHfamLOZynaDVv3Z
+MkAhXBhRbWe2bF701Dnuu/AYfdQmuc3mIp/r6bGEW83IvwmL8W2LolSVY5/lEZp2
+NvyNRc0INDQ5BhQ9u5YAQBQdXri3/oTepoVQXY+MvVdnKR1fSIlgIOVmXhdnf58L
+sjCP579ZnZr3zZsqHwIDAQAB
+-----END PUBLIC KEY-----'); //sample default value
+/*
+    $settings = _webform_pki_encrypt_webform_pki_encrypt_settings(get_current_node_id());
+    if (!empty($settings)) {
+      $public_my_key = $settings['public_key'] ;
+      //$my_private_key = $settings['private_key'];
+    }
+    //drupal_set_message(  "<h1>public_my_key=$public_my_key</h1>");
+*/
+    return $public_my_key;
 }
